c35c43b8dded52879c173a50454ef556f454d56b
Anastasia-Kiseleva12 <pm15.kiseleva1@gmail.com>
2025-02-13 18:01:20 +0300

    add classes

---
+++ GraphicEditor/FigureFabric.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Composition.Hosting;
+using System.Linq;
+
+namespace GraphicEditor
+{
+    class FigureMetadata
+    {
+        public string Name { get; }
+    }
+    public static class FigureFabric
+    {
+        class ImportInfo
+        {
+            [ImportMany]
+            public IEnumerable<Lazy<IFigure, FigureMetadata>> AvailableFigures { get; set; } = [];
+        }
+        static ImportInfo info;
+        static FigureFabric()
+        {
+            var assemblies = new[] { typeof(Circle).Assembly };
+            var conf = new ContainerConfiguration();
+            try
+            {
+                conf = conf.WithAssemblies(assemblies);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            var cont = conf.CreateContainer();
+            info = new ImportInfo();
+            cont.SatisfyImports(info);
+        }
+
+        public static IEnumerable<string> AvailableFigures => info.AvailableFigures.Select(f => f.Metadata.Name);
+        public static IFigure CreateFigure(string FigureName)
+        {
+            return info.AvailableFigures.First(f => f.Metadata.Name == FigureName).Value;
+        }
+    }
+
+    [Export(typeof(IFigure))]
+    [ExportMetadata("Name", nameof(Circle))]
+    public class Circle : IFigure
+    {
+        public void Move(Point vector) => throw new NotImplementedException();
+        public void Rotate(Point center, double angle) => throw new NotImplementedException();
+
+        public Point Center { get; }
+
+        public void Scale(double dx, double dy) => throw new NotImplementedException();
+        public void Scale(Point center, double dr) => throw new NotImplementedException();
+        public void Reflection(Point a, Point b) => throw new NotImplementedException();
+        public IFigure Clone() => throw new NotImplementedException();
+        public IEnumerable<IDrawingFigure> GetAsDrawable() => throw new NotImplementedException();
+        public bool IsIn(Point point, double eps) => throw new NotImplementedException();
+        public IFigure Intersect(IFigure other) => throw new NotImplementedException();
+        public IFigure Union(IFigure other) => throw new NotImplementedException();
+        public IFigure Subtract(IFigure other) => throw new NotImplementedException();
+    }
+}
