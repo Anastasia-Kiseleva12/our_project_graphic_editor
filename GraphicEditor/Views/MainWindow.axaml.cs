79943eab956a3070f2a7a4ef0480c6e8ce6108ac
IvanSidorkin <131771347+IvanSidorkin@users.noreply.github.com>
2025-03-03 15:33:47 +0700

    DrawTriangle + DrawRectangle
    Добавил методы для отрисовки треугольников и прямоугольников

--- GraphicEditor/Views/MainWindow.axaml.cs
+++ GraphicEditor/Views/MainWindow.axaml.cs
@@ -153,6 +153,147 @@ namespace GraphicEditor.Views
                     DrawingCanvas.Children.Add(pointOnCircle);
                 }
             }
+
+            public void DrawTriangle(bool IsSelected, Point Point1, Point Point2, Point Point3)
+            {
+                if (IsSelected)
+                {
+                    var highlightGeometry = new PathGeometry();
+                    var highlightFigure = new PathFigure
+                    {
+                        StartPoint = new Avalonia.Point(Point1.X, Point1.Y)
+                    };
+                    highlightFigure.Segments.Add(new LineSegment { Point = new Avalonia.Point(Point2.X, Point2.Y) });
+                    highlightFigure.Segments.Add(new LineSegment { Point = new Avalonia.Point(Point3.X, Point3.Y) });
+                    highlightFigure.Segments.Add(new LineSegment { Point = new Avalonia.Point(Point1.X, Point1.Y) });
+                    highlightGeometry.Figures.Add(highlightFigure);
+
+                    var highlightShape = new Path
+                    {
+                        Stroke = Brushes.LightBlue,
+                        StrokeThickness = 6,
+                        Data = highlightGeometry
+                    };
+
+                    DrawingCanvas.Children.Add(highlightShape);
+                }
+
+                var triangleGeometry = new PathGeometry();
+                var triangleFigure = new PathFigure
+                {
+                    StartPoint = new Avalonia.Point(Point1.X, Point1.Y)
+                };
+                triangleFigure.Segments.Add(new LineSegment { Point = new Avalonia.Point(Point2.X, Point2.Y) });
+                triangleFigure.Segments.Add(new LineSegment { Point = new Avalonia.Point(Point3.X, Point3.Y) });
+                triangleFigure.Segments.Add(new LineSegment { Point = new Avalonia.Point(Point1.X, Point1.Y) });
+                triangleGeometry.Figures.Add(triangleFigure);
+
+                var triangleShape = new Path
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 2,
+                    Data = triangleGeometry
+                };
+
+                DrawingCanvas.Children.Add(triangleShape);
+
+                if (IsSelected)
+                {
+                    var point1Ellipse = new Ellipse
+                    {
+                        Width = 10,
+                        Height = 10,
+                        Fill = Brushes.Blue,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 1,
+                        Margin = new Thickness(Point1.X - 5, Point1.Y - 5, 0, 0)
+                    };
+
+                    var point2Ellipse = new Ellipse
+                    {
+                        Width = 10,
+                        Height = 10,
+                        Fill = Brushes.Blue,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 1,
+                        Margin = new Thickness(Point2.X - 5, Point2.Y - 5, 0, 0)
+                    };
+
+                    var point3Ellipse = new Ellipse
+                    {
+                        Width = 10,
+                        Height = 10,
+                        Fill = Brushes.Blue,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 1,
+                        Margin = new Thickness(Point3.X - 5, Point3.Y - 5, 0, 0)
+                    };
+
+                    DrawingCanvas.Children.Add(point1Ellipse);
+                    DrawingCanvas.Children.Add(point2Ellipse);
+                    DrawingCanvas.Children.Add(point3Ellipse);
+                }
+            }
+
+            public void DrawRectangle(bool IsSelected, Point TopLeft, Point BottomRight)
+            {
+                if (IsSelected)
+                {
+                    var highlightGeometry = new RectangleGeometry
+                    {
+                        Rect = new Rect(new Avalonia.Point(TopLeft.X, TopLeft.Y), new Avalonia.Point(BottomRight.X, BottomRight.Y))
+                    };
+
+                    var highlightShape = new Path
+                    {
+                        Stroke = Brushes.LightBlue,
+                        StrokeThickness = 6,
+                        Data = highlightGeometry
+                    };
+
+                    DrawingCanvas.Children.Add(highlightShape);
+                }
+
+                var rectangleGeometry = new RectangleGeometry
+                {
+                    Rect = new Rect(new Avalonia.Point(TopLeft.X, TopLeft.Y), new Avalonia.Point(BottomRight.X, BottomRight.Y))
+                };
+
+                var rectangleShape = new Path
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 2,
+                    Data = rectangleGeometry
+                };
+
+                DrawingCanvas.Children.Add(rectangleShape);
+
+                if (IsSelected)
+                {
+                    var topLeftEllipse = new Ellipse
+                    {
+                        Width = 10,
+                        Height = 10,
+                        Fill = Brushes.Blue,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 1,
+                        Margin = new Thickness(TopLeft.X - 5, TopLeft.Y - 5, 0, 0)
+                    };
+
+                    var bottomRightEllipse = new Ellipse
+                    {
+                        Width = 10,
+                        Height = 10,
+                        Fill = Brushes.Blue,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 1,
+                        Margin = new Thickness(BottomRight.X - 5, BottomRight.Y - 5, 0, 0)
+                    };
+
+                    DrawingCanvas.Children.Add(topLeftEllipse);
+                    DrawingCanvas.Children.Add(bottomRightEllipse);
+                }
+            }
         }
         private void Draw()
         {
